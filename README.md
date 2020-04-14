c3174927f57f60deddda3a8bbfabebba2660993d
bunnykiller003 <63455593+bunnykiller003@users.noreply.github.com>
2020-04-14 19:46:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# collisiondetectionalgorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,25 @@
+var movingrect,fixedrect;
+function setup() {
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 50, 50);
+  movingrect =createSprite(400, 200, 80, 80);
+  fixedrect.shapeColor="red"
+  movingrect.shapeColor="red"
+}
+
+function draw() {
+  background(255,255,255); 
+  movingrect.x=mouseX;
+  movingrect.y=mouseY ;
+  if(movingrect.x-fixedrect.x<(movingrect.width/2+fixedrect.width/2)&&fixedrect.x-movingrect.x<(movingrect.width/2+fixedrect.width/2)&&
+  movingrect.y-fixedrect.y<(movingrect.width/2+fixedrect.width/2)&&fixedrect.y-movingrect.y<(movingrect.width/2+fixedrect.width/2)){
+   fixedrect.shapeColor="blue";
+   movingrect.shapeColor="blue";
+
+  }
+  else{
+    fixedrect.shapeColor="red"
+    movingrect.shapeColor="red"
+  }
+  drawSprites();
+}
